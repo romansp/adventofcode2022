9e2a30356ffb1f5228c9695274fe3061804044c9
Raman Paulau <rspaulau@gmail.com>
2022-12-02 01:01:14 -0800

    advent01 part two

--- src/advent01/Program.cs
+++ src/advent01/Program.cs
@@ -1,20 +1,48 @@
-﻿string? line;
+﻿static void PartOne() {
+  string? line;
 
-int maxCalories = 0;
-int totalCalories = 0;
+  int maxCalories = 0;
+  int totalCalories = 0;
 
-while ((line = Console.ReadLine()) != null) {
-  if (string.IsNullOrEmpty(line)) {
-    totalCalories = 0;
-    continue;
+  while ((line = Console.ReadLine()) != null) {
+    if (string.IsNullOrEmpty(line)) {
+      totalCalories = 0;
+      continue;
+    }
+
+    var calories = int.Parse(line);
+    totalCalories += calories;
+
+    if (totalCalories > maxCalories) {
+      maxCalories = totalCalories;
+    }
   }
 
-  var calories = int.Parse(line);
-  totalCalories += calories;
-
-  if (totalCalories > maxCalories) {
-    maxCalories = totalCalories;
-  }
+  Console.WriteLine(maxCalories);
 }
 
-Console.WriteLine(maxCalories);
+static void PartTwo() {
+  string? line;
+
+  int[] topThreeCalories = new int[3];
+  int totalCalories = 0;
+
+  while ((line = Console.ReadLine()) != null) {
+    if (string.IsNullOrEmpty(line)) {
+      if (totalCalories > topThreeCalories[0]) {
+        topThreeCalories[0] = totalCalories;
+        Array.Sort(topThreeCalories);
+      }
+
+      totalCalories = 0;
+      continue;
+    }
+
+    var calories = int.Parse(line);
+    totalCalories += calories;
+  }
+
+  Console.WriteLine(topThreeCalories.Sum());
+}
+
+PartTwo();
