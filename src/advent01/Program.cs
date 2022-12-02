045ec7e96c3aa371b951d1541ff03e366e38b7b8
Raman Paulau <rspaulau@gmail.com>
2022-12-02 00:26:24 -0800

    advent01 solution

---
+++ src/advent01/Program.cs
@@ -0,0 +1,20 @@
+﻿string? line;
+
+int maxCalories = 0;
+int totalCalories = 0;
+
+while ((line = Console.ReadLine()) != null) {
+  if (string.IsNullOrEmpty(line)) {
+    totalCalories = 0;
+    continue;
+  }
+
+  var calories = int.Parse(line);
+  totalCalories += calories;
+
+  if (totalCalories > maxCalories) {
+    maxCalories = totalCalories;
+  }
+}
+
+Console.WriteLine(maxCalories);
